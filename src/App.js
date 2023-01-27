35c2774cbcb8b0cde5ffe6f3bc1e65b13e82fc8b
Mauro Caceres <caceres_mauro22789@outlook.com>
2023-01-27 01:00:13 -0300

    Drawer completed & intersection observer Hero

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Hero from './components/Hero';
 import CardContainer from './components/CardContainer';
 import Footer from './components/Footer';
 import { FloatingWhatsApp } from 'react-floating-whatsapp';
-import Carrousel from './components/Carrousel';
 
 function App() {
   return <>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,32 +1,69 @@
 import { Carousel } from "flowbite-react"
+import { useInView } from "react-intersection-observer"
+
+
 
 const Hero = () => {
+  //Intersection observer Carrousel
+  const {ref: carrouselSlider1, inView: isSlider1Visible} = useInView()
+  const {ref: carrouselSlider2, inView: isSlider2Visible} = useInView()
+  const {ref: carrouselSlider3, inView: isSlider3Visible} = useInView()
 
   return <>
-    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+    <div className="h-[36rem]">
       {/*Edited Controls*/}
       <Carousel leftControl={<div className="-z-1"></div>} rightControl={<div className="-z-1"></div>} border="none" slideInterval={5000}>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white" style={{ backgroundImage: `url(/HeroImg.webp)` }}>
-          Slide 1
+        <div className="flex h-full items-center justify-center bg-cover" style={{ backgroundImage: `url(/HeroImg2.webp)` }}>
+          <div className="flex-col sm:mr-24 md:mr-48 lg:mr-96 justify text-center sm:text-left">
+              
+              <div ref={carrouselSlider1} className={`slider-title ${isSlider1Visible ? 'slider-title-shower' : ''}`}>
+                <h1 className="pb-2 text-3xl md:text-6xl sm:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-200">FGA - Consultora de
+                <strong className="block font-extrabold text-white">Higiene, Seguridad</strong>y Medioambiente</h1>
+              </div>
+              <div className={`slider-text ${isSlider1Visible ? 'slider-text-shower' : ''}`}>
+              <h2 className="md:ml-5 mt-4 max-w-lg sm:text-xl lg:text-2xl sm:leading-relaxed text-white"> <strong>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illotenetur fuga ducimus numquam ea!</strong></h2>
+              </div>
+              <div className={`slider-btn mt-4 ${isSlider1Visible ? 'slider-btn-shower' : ''}`}>
+                <button className="md:ml-5 btn btn-outline btn-info">Contacto</button>
+              </div>
+
+          </div>  
         </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 2
+        <div className="flex h-full items-center justify-center bg-cover" style={{ backgroundImage: `url(/card42.webp)` }}>
+          
+        <div className="flex-col sm:mr-24 md:mr-48 lg:mr-96 justify text-center sm:text-left">
+          <div ref={carrouselSlider2} className={`slider-title ${isSlider2Visible ? 'slider-title-shower' : ''}`}>
+            <div className=" pb-2 text-3xl md:text-6xl sm:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-amber-700 to-amber-100">FGA - Consultora de
+              <strong className="block font-extrabold text-white">Higiene, Seguridad</strong>y Medioambiente
+            </div>
+          </div>
+          <div className={`slider-text ${isSlider2Visible ? 'slider-text-shower' : ''}`}>
+            <div className="md:ml-5 mt-4 max-w-lg sm:text-xl lg:text-2xl sm:leading-relaxed text-white"> <strong>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illotenetur fuga ducimus numquam ea!</strong></div>
+          </div>
+          <div className={`slider-btn mt-4 ${isSlider2Visible ? 'slider-btn-shower' : ''}`}>
+            <button className="md:ml-5 btn btn-outline btn-warning">Contacto</button>
+          </div>
+        </div>  
+
         </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 3
+        <div className="flex h-full items-center justify-center bg-cover" style={{ backgroundImage: `url(/card52.webp)` }}>
+
+          <div className="flex-col sm:mr-24 md:mr-48 lg:mr-96 justify text-center sm:text-left">
+            <div ref={carrouselSlider3} className={`slider-title ${isSlider3Visible ? 'slider-title-shower' : ''}`}>
+              <div className=" pb-2 text-3xl md:text-6xl sm:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-600 via-green-300 to-green-100">FGA - Consultora de
+                <strong className="block font-extrabold text-white">Higiene, Seguridad</strong>y Medioambiente
+              </div>
+            </div>
+            <div className={`slider-text ${isSlider3Visible ? 'slider-text-shower' : ''}`}>
+              <div className="md:ml-5 mt-4 max-w-lg sm:text-xl lg:text-2xl sm:leading-relaxed text-white"> <strong>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illotenetur fuga ducimus numquam ea!</strong></div>
+            </div>
+            <div className={`slider-btn mt-4 ${isSlider3Visible ? 'slider-btn-shower' : ''}`}>
+              <button className="md:ml-5 btn btn-outline btn-success">Contacto</button>
+            </div>
+          </div>  
+
         </div>
       </Carousel>
-    </div>
-
-    <div className="hero min-h-screen" style={{ backgroundImage: `url(/HeroImg.webp)` }}>
-        <div className="hero-overlay bg-opacity-50 shadow-2xl"></div>
-        <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:pr-96 lg:ml-20">
-            <div className="max-w-xl text-center text-neutral-content sm:text-left">
-                <h1 className="text-3xl sm:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-200">FGA - Consultora de
-                <strong className="block font-extrabold text-white">Higiene, Seguridad</strong>y Medioambiente</h1>
-                <h2 className="mt-4 max-w-lg sm:text-xl sm:leading-relaxed text-white"> <strong>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illotenetur fuga ducimus numquam ea!</strong></h2>
-            </div>
-        </div>
     </div>
   </>
 }
