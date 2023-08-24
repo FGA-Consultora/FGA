9f4a0b199cb26034fbde10dbfb0e7af6ccefe5ea
Mauro Caceres <caceres_mauro22789@outlook.com>
2023-08-24 02:27:23 -0300

    Actualización arreglo scroll

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -30,7 +30,7 @@ const Scroll = () => {
   return <>
   
   <button onClick={backToTop} title="Go To Top"
-        className={`transition ease-in-out duration-300 shadow-2xl fixed z-90 bottom-28 right-7 border-0 w-14 h-14 rounded-full drop-shadow-md bg-sky-700 hover:bg-sky-600 text-white text-5xl font-semibold ${showButton ? "visible" : "invisible"}`}>&#8613;</button>
+        className={`shadow-2xl fixed z-90 bottom-28 right-7 border-0 w-14 h-14 rounded-full drop-shadow-md bg-sky-700 hover:bg-sky-600 text-white text-4xl font-semibold ${showButton ? "visible" : "invisible"}`}><svg fill="#e0f2fe" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g data-name="17-Arrow Up"><path d="M16 0a16 16 0 1 0 16 16A16 16 0 0 0 16 0zm0 30a14 14 0 1 1 14-14 14 14 0 0 1-14 14z"/><path d="m15.29 5.29-7 7L9.7 13.7 15 8.41V27h2V8.41l5.29 5.29 1.41-1.41-7-7a1 1 0 0 0-1.41 0z"/></g></svg></button>
   
   </>;
 };
